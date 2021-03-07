c7469ee3c61e9c149791433df805bc6c1180605b
AHCImode <78480842+AHCImode@users.noreply.github.com>
2021-03-07 18:39:53 +0300

    checkout currency format debug

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,6 +2,7 @@ import userEvent from '@testing-library/user-event'
 import React from 'react'
 import "./Checkout.css"
 import { useStateValue } from './StateProvider';
+import Subtotal from './Subtotal';
 
 
 function Checkout() {
@@ -14,7 +15,7 @@ function Checkout() {
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhnytJ1B88NptCJ-OMynShucmiF6SgkkQoTw&usqp=CAU"></img>
             </div>
 
-             <div>
+             <div className="checkout__title">
              <h3>{user?.email }</h3>
                  <h2 className="checkout__title">Your shoping basket</h2>
                  {/*Basket*/}
@@ -24,7 +25,7 @@ function Checkout() {
              </div>
 
              <div className="Checkout__right">
-                <h2>The subtotal go here</h2>
+                <Subtotal/>
              </div>
         </div>
     )
